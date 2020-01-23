e9db179b2acaa9309374eb7ec01d0e9c598a4c9b
frankpettersson <frank.pettersson1@gmail.com>
2020-01-23 13:39:13 +0100

    footer items

--- components/footer.vue
+++ components/footer.vue
@@ -1,7 +1,11 @@
 <template lang="pug">
-    .card-first-last
-        h1.rmR Footer
-        .card-overlay
+  .card-first-last
+    .footer-wrapper
+      h1.footer-title.rmR Country Data
+      a.footer-github(href="https://github.com/frankpettersson/country-data" target="github" rel="noreferrer")
+        img(src="../assets/img/GitHub-Mark-64px.png" alt="github")
+      a.footer-data-source.rmR(href="https://restcountries.eu/" target="api source" rel="noreferrer") Data source
+    .card-overlay
 </template>
 
 <script>
@@ -10,4 +14,52 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.footer-wrapper {
+  display: grid;
+  grid-template-areas:
+    'title'
+    'data-source'
+    'github';
+  grid-gap: 10px;
+  text-align: center;
+
+  .footer-title,
+  .footer-github,
+  .footer-data-source {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .footer-title {
+    grid-area: title;
+  }
+
+  .footer-github {
+    grid-area: github;
+
+    img {
+      margin: 0 auto;
+      width: 64px;
+      border: 1px solid white;
+      border-radius: 50%;
+      background-color: #fff;
+      object-fit: contain;
+    }
+  }
+
+  .footer-data-source {
+    grid-area: data-source;
+  }
+}
+
+@media only screen and (min-width: 1100px) {
+  .footer-wrapper {
+    display: grid;
+    grid-template-areas: 'title data-source github';
+    grid-template-columns: 60% 20% 20%;
+    text-align: left;
+  }
+}
+</style>
